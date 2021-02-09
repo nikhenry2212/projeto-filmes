973ff2e13b3ceb05d046e21c1492d274beada081
Nikolas <nikolaspg@hotmail.com>
2021-02-09 09:54:19 -0300

    first One Commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './home.css'
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filmes: []
+    };
+    this.loadFilmes = this.loadFilmes.bind(this);
+  }
+  componentDidMount() {
+    this.loadFilmes();
+  }
+  loadFilmes() {
+    //https://sujeitoprogramador.com/r-api/?api=filmes/
+    let url = 'https://sujeitoprogramador.com/r-api/?api=filmes'
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ filmes: json })
+
+      })
+
+  }
+
+  render() {
+    return (
+      <div  className="container">
+        <div className="lista-filmes">
+          {this.state.filmes.map((filme) => {
+            return (
+              <article key={filme.id} className="filme">
+                <strong> {filme.nome} </strong>
+                <img src={filme.foto} alt="capa" />
+                <Link to="/">Acessar</Link>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+export default Home;
