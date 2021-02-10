66b3a6e83aa6d12238d9923c1d81aded27c8a3b8
Nikolas <nikolaspg@hotmail.com>
2021-02-10 11:54:06 -0300

    :heavy_check_mark: Finalizado

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,34 @@
+import React, { Component} from 'react';
+import './styles.css'
+
+class Filme extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filme: []
+    }
+  }
+  componentDidMount() {
+    const {id} = this.props.match.params;
+    let url = `https://sujeitoprogramador.com/r-api/?api=filmes/${id}`
+    fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      this.setState({filme: json});
+     
+    })
+  }
+  render() {
+    return (
+      <div className="filme-info">
+        <h1> {this.state.filme.nome} </h1>
+        <img src={this.state.filme.foto} alt="capa"/>
+       {this.state.filme.length !== 0 && 
+       <h3>Sinopse</h3>
+       }
+        {this.state.filme.sinopse}
+      </div>
+    )
+  }
+}
+export default Filme;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Header from './components/Header';
 import Erro from './pages/Erro';
 import Home from './pages/Home';
+import Filme from './pages/Filme';
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
     <Header/>
       <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/filme/:id" component={Filme}/>
         <Route path="*" component={Erro}/>
       </Switch>
     </BrowserRouter>
